14e9f321707ec4f7f3e80b3c2cbc31dc2ab240f4
Saira Nawaz <sairanawaz02@gmail.com>
2022-03-26 06:02:40 +0800

    First Commit

---
+++ app/src/main/java/com/sg2d/app/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.sg2d.app.utils;
+
+public class Constants {
+
+    public static final String API_URL = "";
+    public static final String API_TOKEN = " ";
+
+    public static final String TYPE_FILE = "FILE";
+    public static final String TYPE_TEXT = "TEXT";
+}
